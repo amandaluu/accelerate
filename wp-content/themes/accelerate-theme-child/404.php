a82adf20370caa0a7d3c13429bf5dc30a87bec28
Lulu <mandydl33@gmail.com>
2016-11-11 19:09:48 -0800

    merging 404 branch into master

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+<section class="404-page">
+  <div class="site-content">
+    <div class="content-wrapper clearfix">
+      <div class="404-image">bloody hell</div>
+      <div class="404-title">
+        <h2>Whoops!</h2>
+        <p>Sorry, this page no longer exists, never existed or has been moved. We feel like complete jerks for totally misleading you.</p>
+        <p>Feel free to take a look around our <span><a href="">blog</a></span> or some of our <span><a href="">featured work.</a></span></p>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
